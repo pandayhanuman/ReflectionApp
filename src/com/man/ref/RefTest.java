abdb1f45c5eab06c8b29145f55dc0aac5f9aab2a
hanuman <panday.hanuman@gmail.com>
2015-05-01 22:55:00 +0530

    modify comment

--- src/com/man/ref/RefTest.java
+++ src/com/man/ref/RefTest.java
@@ -5,7 +5,7 @@ import java.lang.reflect.Method;
 public class RefTest {
 
 	public static void main(String[] args) {
-		System.out.println("----------------------------------------");
+		System.out.println("-------------------=======---------------------");
 		Class cl=Simple.class;
 		
 		try { 
